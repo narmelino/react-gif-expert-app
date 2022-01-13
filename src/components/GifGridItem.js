ba52e76d1322ee08eb9dac69e01a5f2286521e92
Nicolás Armelino <nicolas.armelino@mercadolibre.com>
2022-01-13 14:38:09 -0300

    Configuración de pruebas y prueba de GifGridItem

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,8 +1,15 @@
+import PropTypes from 'prop-types';
+
 const GifGridItem = ({title, url}) => (
   <div className="card animate__animated animate__fadeIn">
     <img src={url} alt={title} />
     <p>{title}</p>
   </div>
 );
+
+GifGridItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
 
 export default GifGridItem;
---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,30 @@
+import { shallow } from 'enzyme';
+import GifGridItem from '../../components/GifGridItem';
+
+describe('Pruebas de GifGritItem', () => {
+  const title = 'One Punch Man Preview GIF';
+  const url = 'https://media2.giphy.com/media/arbHBoiUWUgmc/giphy.gif?cid=bce6e586dgm3omnh06gjms04coduyi97cr9db3c968kc7odu&rid=giphy.gif&ct=g';
+  const className = 'animate__fadeIn';
+  
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
+  
+  test('Debe renderizar el componente', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Debe tener un párrafo con el title', () => {
+    const p = wrapper.find('p');
+    expect(p.text()).toBe(title);
+  });
+
+  test('Debe tener la imagen igual al url y el alt igual al title', () => {
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toBe(url);
+    expect(img.prop('alt')).toBe(title);
+  });
+
+  test('Debe tener un div con la clase fadeIn de animate.css', () => {
+    const div = wrapper.find('div');
+    expect(div.hasClass(className)).toBe(true);
+  });
+});

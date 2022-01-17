8bd3b076c17b78398078e97e6e75933ee766b4eb
Nicolás Armelino <nicolas.armelino@mercadolibre.com>
2022-01-17 14:01:29 -0300

    Actualizar test de hook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,14 +2,25 @@ import { renderHook } from '@testing-library/react-hooks';
 import useFetchGifs from "../../hooks/useFetchGifs";
 
 describe('Pruebas con el hook useFetchGifs', () => {
-  test('Debe retornar el estado inicial.', () => {
+  test('Debe retornar el estado inicial.', async() => {
     const category = 'Goku';
-    const { result } = renderHook(() => useFetchGifs(category));
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs(category));
     const { loanding, data} = result.current;
 
-    console.log(loanding, data);
+    await waitForNextUpdate();
 
     expect(loanding).toBe(true);
     expect(data).toEqual([]);
+  });
+
+  test('Debe retornar un arreglo de imágenes', async() => {
+    const category = 'Goku';
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs(category));
+    await waitForNextUpdate();
+    
+    const { loanding, data} = result.current;
+
+    expect(loanding).toBe(false);
+    expect(data.length).toBe(10);
   });
 });
